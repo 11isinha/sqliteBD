763d056599d84c76f0c2d9fd7e5c435dd1751156
isinha🫧 <isabella.r.rosa@aluno.senai.br>
2025-08-26 15:08:18 -0300

    atualização

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 //importa a biblioteca express
-const express = require ('express');
+const { json } = require('body-parser');
+const express =require('express');
 //importa a biblioteca SQLite3
-const sqlite3 = require ('sqlite3').verbose();
+const sqlite3 = require('sqlite3').verbose();
 //cria uma instacia do aplicativo express 'app' será o servidor
 const app = express();
 //define a porta que o servidor irá escutar por requisições
@@ -9,11 +10,10 @@ const port = 3000;
 //middleware é um software que fica no meio do caminho da requisição
 app.use(express.json());
 
-
 const db = new sqlite3.Database('./meubanco.db', (err) =>{
     //se err não for nulo significa que ocorreu um erro na conexão
     if (err){
-        console.error('Erro ao conectar ao banco de dados', err.message);
+        console.error('Erro ao conectar banco de dados', err.message);
         //se a conexão for bem sucedida
     }else{
         console.log('Conectado ao banco de dados SQLite');
@@ -22,128 +22,130 @@ const db = new sqlite3.Database('./meubanco.db', (err) =>{
         id INTEGER PRIMARY KEY AUTOINCREMENT, -- 'id' é chave primaria
         nome TEXT NOT NULL, -- 'nome' é um texto e não pode ser nulo
         email TEXT UNIQUE NOT NULL -- 'email' é um texto e não pode ser nulo
-    )`, (err) =>{
-        if(err){
-            console.error('Erro ao criar tabela', err.message);
-        }else{
-            console.log('Tabela usuarios pronta');
-        }
-    });
+        )`, (err) =>{
+            if(err){
+                console.error('Erro ao criar tabela', err.message);
+            }else{
+                console.log('Tabela usuarios pronta');
+            }
+        });
 });
 
-//rotas CRUD (Creat, Read, Update, Delete)
-//POST = Create
-app.post('/usuarios',(req,res)=>{
+//rotas CRUD (Creat, Read, Uptade, Delete)
+//POST = Creat
+app.post('/usuarios', (req, res) =>{
     //CREATE: rota para criar um novo usuario
-    //app.post()define que esta rota responde a requisições HTTP tipo POST 
+    //app.post() define que esta rota responde a requisições HTTP tipo POST
     //o caminho da rota é '/usuarios'
-    //extrai 'nome' e 'email' do corpo da requisição (req.body)
-    const {nome,email}= req.body;
-    //validação simples para garantir que nome e email foram enviados 
+    //extrai 'nome' e 'email' do corpo da requisição(req.body)
+    const {nome, email} = req.body;
+    //validação simple para garantir que nome e email foram enviados
     if (!nome || !email){
-        //se faltar algum tipo campo, retorna erro 400(Bad Request)
+        //se faltar algum campo, retorna erro 400(Bad Request)
         return res.status(400).json({error: 'Nome e email são obrigatórios'});
     }
     //comando SQL para inserir um novo registro na tabela 'usuarios'
     //od '?' são placeholders que serão substituidos pelos valores digitados
-    const sql= `INSERT INTO usuarios (nome, email) VALUES (?, ?)`;
+    const sql = `INSERT INTO usuarios (nome, email) VALUES (?, ?)`;
 
-    db.run(sql, [nome,email], function (err){
-        //se ocorrer erro pode ser emial já existe retorna erro 500
-        if (err){
+    db.run(sql, [nome, email], function(err){
+        //se ocorrer ero pode ser email já existe retorna erro 500
+        if (err) {
             console.error(err.message);
-            return res.status(500).json({error:'Erro ao inserir usuario ou email já existe'});
+            return res.status(500).json({error: 'Erro ao inserir usuario ou email já existe'});
         }
         res.status(201).json({
-            //mensagem 201 retorna criado com sucesso
-            message: 'Usuário criado com sucesso! ',
+            //mensagem 201  retorna criado com sucesso
+            message: 'Usuário criado com sucesso!',
             id: this.lastID
         });
     });
 });
 
-//GET= Read
-app.get('/usuario',(req,res)=>{
-    const sql= `SELECT * FROM usuarios`;
-    db.all(sql,[],(err,rows)=>{
-      if(err) {
-        console.error(err.message); 
-        return res.status(500).json({error:'Erro ao buscar usuários'});
-       }
-       res.json({
-        message:'Usuários listados com sucesso!',
-        data: rows
-       });
+//GET = Read
+app.get('/usuarios', (req, res) =>{
+    const sql = `SELECT * FROM usuarios`;
+    db.all(sql, [], (err, rows) =>{
+        if (err) {
+            console.error(err.message);
+            return res.status(500).json({error: 'Erro ao buscar usuários'});
+        }
+        res.json({
+            message: 'Usuários listados com sucesso!',
+            data: rows
+        });
     })
 });
 
-//GET = Read com ID 
-app.get('/usuarios/:id',(req,res)=>{
-    const {id}= req.params;
-    const sql= `SELECT * usuarios WHERE id=?`;
-    db.get(sql,[id], (err,row)=>{
-        if(err){
+//GET = Read com ID
+app.get('/usuarios/:id', (req, res) => {
+    const {id} = req.params;
+    const sql = `SELECT * FROM usuarios WHERE  id = ?`;
+    db.get(sql, [id], (err, row) =>{
+        if (err){
             console.error(err.message);
             return res.status(500).json({error: 'Erro ao buscar usuário'});
         }
         if(row) {
             res.json({
-                message: 'Usuário encontrado!',
+                message: 'Usuario encontrado!',
                 data: row
             });
         }else{
-            res.status(404).json({error:'Usuário não encontrado'});
-        }   
-        });
+            res.status(404).json({error: 'Usuário não encontrado'});
+        } 
     });
+});
 
-    //PUT = Update
-    app.put('/usuarios/:id',(req, res)=>{
-        const {id}= req.params;
-        const {nome,email} =req.body;
-        if (!nome || !email){
-            return res.status(400).json({ error: 'Nome e email são obrigatórios'});
-        }
-        const sql = `UPDATE usuarios SET nome = ?, email = ? WHERE id = ?`;
-        //executa o comando de atualização
-        db.run(sql,[nome,email,id], function (err){
-            if (err){
-                console.error(err.message);
-                return res.status(500).json({error:'Erro ao atualizar usuario'});
-            }
-            if(this.changes>0){
-                res.status(404).json({error:'Usuario não encontrado'});
-            }
-        });
-     });
-
-     //DELETE = exclui
-     app.delete('/usuarios/:id', (req,res)=>{
-        const {id} = req.params;
-        const sql= `DELETE FROM usuarios WHERE id=? `;
-        db.run (sql, [id], function(err){
-            if(err){
+//PUT = Update
+app.put('/usuarios/:id', (req, res) =>{
+    const {id} = req.params;
+    const {nome, email} = req.body;
+    if (!nome || !email){
+        return res.status(400).json({ error: 'Nome e email são obrigatórios'});
+    }
+    const sql = `UPDATE usuarios SET nome = ?, email = ? WHERE id = ?`;
+    //executa o comando de atualização
+    db.run(sql, [nome, email, id], function(err){
+        if (err){
             console.error(err.message);
-            return res.status(500).json({error:'Erro ao deletar usuário'});
-            }
-            if (this.changes>0){
-                res.json({message:`Usuário com ${id} deletado com sucesso`});
-            }else{
-                res.status(404).json({error: 'Usuário não encontrado'});
-            }
-        });
-     });
+            return res.status(500).json({error: 'Erro ao atuaizar usuario'});
+        }
+        if (this.changes > 0){
+            res.json({message: 'Usuário atualizado com sucesso'});
+        } else {
+            res.status(404).json({error: 'Usuário não encontrado'});
+        }
+    });
+});
 
-     //inicia o servidor e faz escuta da porta definida
-     app.listen(port,()=>{
-        console.log(`Servidor rodando em https://localhost:${port}`);
-     }); 
-     process.on ('SIGINT', ()=>{
-        db.close((err)=>{
-             if (err){
+//DELETE  = exclui
+app.delete('/usuarios/:id', (req, res) =>{
+    const {id} = req.params;
+    const sql = `DELETE FROM usuarios WHERE id = ?`;
+    db.run(sql, [id], function(err){
+        if (err){
+            console.error(err.message);
+            return res.status(500).json({error: 'Erro ao deletar usuário'});
+        }
+        if (this.changes > 0) {
+            res.json({message: `Usuário com ${id} deltado com sucesso`});
+        }else{
+            res.status(404).json({error: 'Usuário não encontrado'});
+        }     
+    });
+});
+
+//inicia o servidor e faz escuta da porta definida
+app.listen(port,() =>{
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
+process.on('SIGINT', () =>{
+    db.close((err) => {
+        if (err){
             console.error(err.message);
         }
-        console.log ('Conexão com o banco de dados SQLite fechada');
+        console.log('Conexão com o banco de daods SQLite fechada');
         process.exit(0);
-        });
-     });
+    });
+});
